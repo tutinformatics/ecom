7b3ff03233c2230104be6e1b847b4b0782de9d68
eghunt <eghunt@ttu.ee>
2020-05-08 12:41:14 +0300

    Added date convert

--- src/pages/orders/list/orders.ts
+++ src/pages/orders/list/orders.ts
@@ -2,14 +2,25 @@ import {Router} from 'aurelia-router';
 import {inject} from 'aurelia-framework'
 import {OrderReportViewService} from '../../../service/order-report-view-service'
 import {OrderReportView} from '../../../model/order-report-view'
+import {TimeUtils} from "../../../util/time-utils";
 
 @inject (Router, OrderReportViewService)
 export class Orders{
 
   orderReportViews: OrderReportView [] = []
+  orderDate = null
 
   constructor(private router: Router, private orderReportViewService: OrderReportViewService) {
     this.loadOrderReportView()
+    this.covertTime()
+  }
+
+  covertTime(){
+    var i;
+    for (i in this.orderReportViews) {
+      console.log(i.orderDate)
+      i.orderDate = TimeUtils.parse(i.orderDate)
+    }
   }
 
   loadOrderReportView() {
