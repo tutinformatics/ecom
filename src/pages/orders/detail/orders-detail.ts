5a3c70ebd588dd5a0d8096b9da8f37ec2134dd48
eghunt <eghunt@ttu.ee>
2020-05-17 14:36:27 +0300

    Made buttons to approve or take back order and item status.

--- src/pages/orders/detail/orders-detail.ts
+++ src/pages/orders/detail/orders-detail.ts
@@ -3,19 +3,42 @@ import {OrderContact} from "../../../model/order-contact";
 import {OrderContactService} from "../../../service/order-contact-service";
 import {OrderReportView} from "../../../model/order-report-view";
 import {OrderReportViewService} from "../../../service/order-report-view-service";
+import {OrderButton} from "../../../model/order-button";
 
 @inject (OrderContactService, OrderReportViewService)
 export class OrdersDetail {
 
+  orderButton: OrderButton
   orderContacts: OrderContact [] = []
   orderReportViews: OrderReportView [] = []
 
   constructor(private orderContactService: OrderContactService, private orderReportViewService: OrderReportViewService){
+    this.orderButton = new OrderButton()
   }
 
   activate(params) {
     this.loadOrderContacts(params.id)
     this.loadOrderReportView(params.id)
+    if (this.orderReportViews[params.id].orderStatus == "Approved"
+      || this.orderReportViews[params.id].orderStatus == "Created") {
+      this.orderButton.orderStatus = "Kinnita"
+    } else {
+      this.orderButton.orderStatus = "Võta tagasi"
+    }
+    if (this.orderReportViews[params.id].itemStatus == "Approved"
+      || this.orderReportViews[params.id].itemStatus == "Created") {
+      this.orderButton.itemStatus = "Kinnita"
+    } else {
+      this.orderButton.itemStatus = "Võta tagasi"
+    }
+  }
+
+  changeOrderStatus() {
+
+  }
+
+  changeItemStatus() {
+
   }
 
   loadOrderContacts(id) {
